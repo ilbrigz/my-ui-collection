6bfc0ea821759f83291794fdc12b238c6f50278b
ilogirb23 <ilogirb23@yahoo.com>
2024-06-04 06:40:59 +0800

    updated hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -20,7 +20,7 @@ function Hero({}: Props) {
               className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
               href="#"
             >
-              Get Started
+              Get Started!
             </a>
 
             <a
